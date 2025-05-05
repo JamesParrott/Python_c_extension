922f3bd7c8e865b7ae315e9b3e442f52158b5b19
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-05-05 14:51:43 +0100

    Update spammodule.c

--- C_Ext/spammodule.c
+++ C_Ext/spammodule.c
@@ -18,6 +18,18 @@ static PyObject *SpamError;
 
 // "your module's initialization function".
 //  "initializes" SpamError "with an exception object".
+//
+// "When the Python program imports module spam for 
+// the first time, PyInit_spam() is called. "
+// "It calls PyModule_Create(), which returns a module object, 
+// and inserts built-in function objects into the newly created 
+// module based upon the table (an array of PyMethodDef structures) 
+// found in the module definition. PyModule_Create() returns a 
+// pointer to the module object that it creates. It may abort 
+// with a fatal error for certain errors, or return NULL if the 
+// module could not be initialized satisfactorily. The init function 
+// must return the module object to its caller, so that it then gets 
+// inserted into sys.modules."
 PyMODINIT_FUNC
 PyInit_spam(void)
 {
@@ -51,7 +63,11 @@ PyInit_spam(void)
 
     return m;
 }
-
+// When embedding Python, the PyInit_spam() function is not called 
+// automatically unless there's an entry in the PyImport_Inittab table. 
+// To add the module to the initialization table, use 
+// PyImport_AppendInittab(), optionally followed by an import of 
+// the module...
 
 // the C function that will be called when the 
 // Python expression spam.system(string) is evaluated
@@ -99,4 +115,43 @@ spam_system(PyObject *self, PyObject *args)
     // which means "error" in most contexts, as we have seen."
 
 }
+
+
+// https://docs.python.org/3/extending/extending.html#the-module-s-method-table-and-initialization-function
+// "how spam_system() is called from Python programs. 
+// First, we need to list its name and address in a "method table":"
+
+static PyMethodDef SpamMethods[] = {
+    ...
+    {"system",  spam_system, METH_VARARGS,
+     "Execute a shell command."},
+    ...
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+// "Note the third entry (METH_VARARGS). This is a flag telling the 
+// interpreter the calling convention to be used for the C function. 
+// It should normally always be 
+// METH_VARARGS or METH_VARARGS | METH_KEYWORDS; 
+// a value of 0 means that an obsolete variant of 
+// PyArg_ParseTuple() is used. "
+//
+// " The METH_KEYWORDS bit may be set in the third field if keyword 
+// arguments should be passed to the function. In this case, 
+// the C function should accept a third PyObject * parameter which 
+// will be a dictionary of keywords. Use PyArg_ParseTupleAndKeywords() 
+// to parse the arguments to such a function. "
+
+// "The method table must be referenced in the module definition structure:"
+
+static struct PyModuleDef spammodule = {
+    PyModuleDef_HEAD_INIT,
+    "spam",   /* name of module */
+    spam_doc, /* module documentation, may be NULL */
+    -1,       /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+    SpamMethods
+};
+
+// spammodule referred to in PyInit_spam, where it is passed to the interpreter.
 
